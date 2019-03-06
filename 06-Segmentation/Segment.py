f2d19cdddb8f27fa0d4949ea26cf20f3a74eb235
Mauricio Neira <m.neira10@uniandes.edu.co>
2019-03-06 16:51:13 -0500

    basic segmentation algorithms

--- 06-Segmentation/Segment.py
+++ 06-Segmentation/Segment.py
@@ -8,137 +8,159 @@ from sklearn.cluster import AgglomerativeClustering
 from sklearn.mixture import GaussianMixture
 from skimage.morphology import watershed
 from sklearn.feature_extraction.image import grid_to_graph
+from scipy import ndimage
+from skimage.feature import peak_local_max
+from skimage import filters
 
 
+# Colour Spaces
 
-# Initialize variables
-filename = 'BSDS_small/train/22090.jpg'
-colordos = 'lab'
-method = 'kmeans'
-n_clusters = 3
-
-## Colour Spaces
 
 def moveColor(image, color):
-    choices={'rgb':-10,'lab':cv2.COLOR_RGB2LAB,'hsv':cv2.COLOR_RGB2HSV,'rgb+xy':-10,'lab+xy':cv2.COLOR_RGB2LAB,'hsv+xy':cv2.COLOR_RGB2HSV,'gray': cv2.COLOR_RGB2GRAY}
-
+    choices = {'rgb': -10, 'lab': cv2.COLOR_RGB2LAB, 'hsv': cv2.COLOR_RGB2HSV, 'rgb+xy': -10,
+               'lab+xy': cv2.COLOR_RGB2LAB, 'hsv+xy': cv2.COLOR_RGB2HSV, 'gray': cv2.COLOR_RGB2GRAY}
 
     transform = choices[color]
+    print('Transforming from rgb to {}'.format(transform))
     if transform != -10:
         image = cv2.cvtColor(image, transform)
     if color.endswith('xy'):
-        hlen,wlen,zlen = image.shape
-        
-        hm = np.zeros((hlen,wlen,1))
-        wm = np.zeros((hlen,wlen,1))
+        print('Adding xy spatial dimensions')
+        hlen, wlen, zlen = image.shape
+
+        hm = np.zeros((hlen, wlen, 1))
+        wm = np.zeros((hlen, wlen, 1))
         for i in range(hlen):
-            hm[i,:,:] +=i
+            hm[i, :, :] += i
         for j in range(wlen):
-            wm[:,j,:] +=j
+            wm[:, j, :] += j
 
-        image = np.append(image,hm,axis=2)
-        image = np.append(image,wm,axis=2)
-        print(image.shape)
+        image = np.append(image, hm, axis=2)
+        image = np.append(image, wm, axis=2)
 
-        # image = np.concatenate((image,wm),axis=2)
+    return normalizeImage(image)
 
-
-
-    return normalizeImage(image)    
-    
 
 def normalizeImage(image):
-    print("Image dimensions {} ".format(image.shape))
-    # if(len(image.shape)>2):
-    #     for i in range(image.shape[2]):
-    #         fig = plt.figure(figsize=(20,15))
-    #         plt.hist(image[:,:,i].flatten())
-    #         plt.show()
-    #         plt.close()
-    normalized_image = cv2.normalize(image, None, alpha = 0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    # print("Image dimensions {} ".format(image.shape))
+    normalized_image = cv2.normalize(
+        image, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
     return normalized_image
-## Clustering
 
 
- 
-def clusterImage(image, method,k):
-    # choices=['kmeans','gmm',hierarchical','watershed']
-    #scikit-learn expects 2D num arrays for the training set for a fit function.
-    #stackoverflow/questions/34972142
-    prediction = None 
-    channels = image.shape[2]
-    train = image.reshape((nx*ny,channels)) # vectors must live in RGB=R^3.
+def clusterImage(image, method, k):
+    prediction = None
+    nx, ny, channels = image.shape
+    train = image.reshape((nx*ny, channels))  # vectors must live in RGB=R^3.
     if method == 'kmeans':
         kmeans = KMeans(k)
         kmeans.fit(train)
-        prediction = kmeans.labels_.astype(np.int).reshape(nx,ny)
+        prediction = kmeans.labels_.astype(np.int).reshape(nx, ny)
     if method == 'gmm':
         gmm = GaussianMixture(k)
         gmm.fit(train)
-        prediction = gmm.predict(train).reshape(nx,ny)
+        prediction = gmm.predict(train).reshape(nx, ny)
     if method == 'hierarchical':
-        connectivity = grid_to_graph(image.shape[0],image.shape[1])
-        X = np.reshape(cv2.cvtColor(image, cv2.COLOR_RGB2GRAY), (-1, 1))
+        connectivity = grid_to_graph(image.shape[0], image.shape[1])
+        X = np.reshape(cv2.cvtColor(
+            image[:, :, :3], cv2.COLOR_RGB2GRAY), (-1, 1))
 
         ward = AgglomerativeClustering(n_clusters=k,
-                linkage='ward', connectivity=connectivity)
+                                       linkage='ward', connectivity=connectivity)
         ward = ward.fit(X)
-        prediction = ward.labels_.astype(np.int).reshape(nx,ny)
-        
-#   TODO
-#    if method == 'watershed':
-#        return watershed
+        prediction = ward.labels_.astype(np.int).reshape(nx, ny)
+
+    if method == 'watershed':
+        gray = np.mean(image, axis=2)
+
+        seeds = np.zeros(gray.shape, dtype=int)
+        edges = filters.sobel(gray)
+        coordinates = peak_local_max(gray, min_distance=20)
+        print(coordinates)
+
+        vals = np.zeros(coordinates.shape[0])
+        for i in range(len(vals)):
+            vals[i] = gray[coordinates[i][0]][coordinates[i][1]]
+
+        ii = vals.argsort()
+        coordinates = coordinates[ii]
+        if k > len(vals):
+            k = len(vals)
+        for i in range(k):
+            seeds[coordinates[i][0], coordinates[i][1]] = i+1
+
+        prediction = watershed(edges, seeds)
+
     return prediction
 
-#def buildPrediction(labels,[x,y,z]):
-#    if hasattr(,'labels_'):
-#        prediction = 
-#    return
 
-def groundtruth():
+def groundtruth(filename):
     import scipy.io as sio
-    gth = sio.loadmat(filename.replace('jpg','mat'))
-    seg = gth['groundTruth'][0,5][0][0]['Segmentation']
+    gth = sio.loadmat(filename.replace('jpg', 'mat'))
+    seg = gth['groundTruth'][0, 5][0][0]['Segmentation']
     return seg
 
-## Debugging
 
-image = imageio.imread(filename)
-nx, ny, channels = image.shape
-# rgbImage = moveColor(image,'rgb')
-# hsvImage = moveColor(image,'hsv')
-# labImage = moveColor(image,'lab')
-# grayImage = moveColor(image,'gray')
+def evalMetric(originalLabels, predictedLabels):
+    uniqueValsOriginal = len(np.unique(originalLabels))
+    uniqueValsPredicted = len(np.unique(predictedLabels))
+    # eval matrix init
+    m = np.zeros((uniqueValsOriginal, uniqueValsPredicted))
+    # image dims
+    nx, ny = originalLabels.shape
+    # build eval matrix
+    for i in range(nx):
+        for j in range(ny):
+            m[originalLabels[i][j]-1][predictedLabels[i][j]-1] += 1
+    # get max per row
+    ans = 0
+    for i in range(uniqueValsOriginal):
+        ans += np.max(m[i])
+    # normalize and return
+    return ans/(nx*ny)
 
-# figure = plt.figure(1)
-# axis = figure.add_subplot(3,1,1)
-# plt.imshow(labImage)
-# axis = figure.add_subplot(3,1,2)
-# plt.imshow(hsvImage)
-# axis = figure.add_subplot(3,1,3)
-# plt.imshow(grayImage,cmap='gray')
-# plt.show()
 
-# kmeans = clusterImage(image,'kmeans')
-#Al final no recupero los centroides; unicamente las clases.
-#kmeans_centers = kmeans.cluster_centers_
-#kmeans_labels = kmeans.labels_
-gmm = clusterImage(image,'gmm',3)
-# No memory for HCL :(
-hcl = clusterImage(image,'hierarchical',5)
-segmentation = groundtruth()
+def segmentByClustering(image, color, k, method):
+    print('Segmenting image. Colorspace = {} Method = {} k = {}'.format(color,method,k))
+    image = moveColor(image, color)
+    print('Clustering')
+    segmentedImage = clusterImage(image, method, k)
+    return segmentedImage
 
-figure = plt.figure(2)
-axis = figure.add_subplot(4,1,1)
-plt.imshow(hcl)
 
-axis = figure.add_subplot(4,1,2)
-plt.imshow(gmm)
-axis = figure.add_subplot(4,1,3)
-plt.imshow(segmentation)
-axis = figure.add_subplot(4,1,4)
-plt.imshow(image)
-plt.show()
+if __name__ == '__main__':
+    # Initialize variables
+    filename = 'BSDS_small/train/22090.jpg'
+    colordos = 'lab'
+    method = 'kmeans'
+    n_clusters = 3
 
-def segmentByClustering( rgbImage, colorSpace, clusteringMethod, numberOfClusters):
-    return segmentation
+    image = imageio.imread(filename)
+
+    gmm = segmentByClustering(image, 'lab+xy', 15, 'gmm')
+    hcl = segmentByClustering(image, 'lab+xy', 15, 'hierarchical')
+    ws = segmentByClustering(image, 'lab+xy', 15, 'watershed')
+    gt = groundtruth(filename)
+
+    print('GMM')
+    print(evalMetric(gt, gmm))
+    print('HCL')
+    print(evalMetric(gt, hcl))
+
+    figure = plt.figure(2)
+    axis = figure.add_subplot(5, 1, 1)
+    plt.imshow(hcl)
+    plt.title('heirarchical')
+    axis = figure.add_subplot(5, 1, 2)
+    plt.imshow(gmm)
+    plt.title('gaussians')
+    axis = figure.add_subplot(5, 1, 3)
+    plt.imshow(gt)
+    plt.title('ground truth')
+    axis = figure.add_subplot(5, 1, 4)
+    plt.imshow(image)
+    plt.title('original')
+    axis = figure.add_subplot(5, 1, 5)
+    plt.imshow(ws)
+    plt.title('watershed')
+    plt.show()
--- 06-Segmentation/main.py
+++ 06-Segmentation/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 
+
 def imshow(img, seg, title='Image'):
     import matplotlib.pyplot as plt
     plt.imshow(img, cmap=plt.get_cmap('gray'))
@@ -10,36 +11,43 @@ def imshow(img, seg, title='Image'):
     plt.title(title)
     plt.axis('off')
     plt.show()
-    
+
+
 def groundtruth(img_file):
     import scipy.io as sio
     img = imageio.imread(img_file)
-    gt=sio.loadmat(img_file.replace('jpg', 'mat'))
-    segm=gt['groundTruth'][0,5][0][0]['Segmentation']
+    gt = sio.loadmat(img_file.replace('jpg', 'mat'))
+    segm = gt['groundTruth'][0, 5][0][0]['Segmentation']
     imshow(img, segm, title='Groundtruth')
-    
+
+
 def check_dataset(folder):
     import os
     if not os.path.isdir(folder):
         os.system('wget http://157.253.196.67/BSDS_small.zip')
         os.system('unzip BSDS_small')
 
+
 if __name__ == '__main__':
     import argparse
     import imageio
-    from Segment import segmentByClustering # Change this line if your function has a different name
+    # Change this line if your function has a different name
+    from Segment import segmentByClustering
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--color', type=str, default='rgb', choices=['rgb', 'lab', 'hsv', 'rgb+xy', 'lab+xy', 'hsv+xy']) # If you use more please add them to this list.
+    # If you use more please add them to this list.
+    parser.add_argument('--color', type=str, default='rgb',
+                        choices=['rgb', 'lab', 'hsv', 'rgb+xy', 'lab+xy', 'hsv+xy'])
     parser.add_argument('--k', type=int, default=4)
-    parser.add_argument('--method', type=str, default='watershed', choices=['kmeans', 'gmm', 'hierarchical', 'watershed'])
+    parser.add_argument('--method', type=str, default='kmeans',
+                        choices=['kmeans', 'gmm', 'hierarchical', 'watershed'])
     parser.add_argument('--img_file', type=str, required=True)
-	
+
     opts = parser.parse_args()
 
     check_dataset(opts.img_file.split('/')[0])
 
     img = imageio.imread(opts.img_file)
-    seg = segmentByClustering(rgbImage=img, colorSpace=opts.color, clusteringMethod=opts.method, numberOfClusters=opts.k)
+    seg = segmentByClustering(img, opts.color, opts.k, opts.method)
     imshow(img, seg, title='Prediction')
     groundtruth(opts.img_file)
